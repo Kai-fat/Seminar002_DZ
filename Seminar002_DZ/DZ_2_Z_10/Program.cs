cf154900ada775003e8c0883058360000dbe2076
Khabukin <dihab@mail.ru>
2022-11-30 00:17:15 +0300

    Seminar002_DZ 2 Z13

---
+++ Seminar002_DZ/DZ_2_Z_10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+//показывает вторую цифру этого числа. Через строку решать нельзя.
+Console.WriteLine("Введите трехзначное число");
+int N = Convert.ToInt32(Console.ReadLine());
+int InputSecondNumber(int N)
+//int secondNumber = InputSecondNumber(N);
+{
+    int count;
+    count = N % 100 / 10;
+    return count;
+}
+int secondNumber = InputSecondNumber(N);
+Console.Write($"Вторая цифра из {N} = {secondNumber}");
