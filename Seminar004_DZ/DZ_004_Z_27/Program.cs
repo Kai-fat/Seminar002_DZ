f8a3a08faf77f83aff21325f49c56bf5e62796e2
Khabukin <dihab@mail.ru>
2022-12-06 14:36:36 +0300

    Seminar004_DZ_Z-25-27-29

---
+++ Seminar004_DZ/DZ_004_Z_27/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+// Через строку решать нельзя.
+// 452 -> 11  82 -> 10   9012 -> 12
+
+Console.WriteLine("Введите  число");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+Console.WriteLine($"{Index(num)}");
+
+int Index(int num)
+{
+    for (int i = num; i != 0; i = i / 10) sum +=i % 10;
+        return sum;
+}
